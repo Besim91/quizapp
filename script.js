40564ecd729a737feac93e0f91a4d1c49d6b7090
Besim Mustafi <besimnaim@gmail.com>
2024-01-29 13:26:40 +0100

    Add JSON and questions

--- script.js
+++ script.js
@@ -1,143 +1,83 @@
-function onload() {
-  start();
-  // questionFooter();
-  // renderQuestionAnswer();
+let quizJson = [
+  {
+    question: "What is the name of the inventor of JavaScript?",
+    answer_1: "Bruce Lee",
+    answer_2: "Steve Jobs",
+    answer_3: "Brendan Eich",
+    answer_4: "Konrad Zuse",
+    right_answer: "3",
+  },
+  {
+    question: "What is true?",
+    answer_1: "0 == undefined",
+    answer_2: "0 == null",
+    answer_3: "0 >= undefined",
+    answer_4: "0 >= null",
+    right_answer: "4",
+  },
+  {
+    question: "What was JavaScript's maiden name shortly after she was born?",
+    answer_1: "Mocha",
+    answer_2: "Micha",
+    answer_3: "Mecha",
+    answer_4: "Mucha",
+    right_answer: "1",
+  },
+  {
+    question:
+      "Who sets the technical specifications and guidelines for the World Wide Web?",
+    answer_1: "Microsoft",
+    answer_2: "Mozilla",
+    answer_3: "Google",
+    answer_4: "World Wide Web Consortium",
+    right_answer: "4",
+  },
+  {
+    question: "What is the correct HTML element for playing audio files?",
+    answer_1: "<audio>",
+    answer_2: "<mp3>",
+    answer_3: "<sound>",
+    answer_4: "<music>",
+    right_answer: "1",
+  },
+  {
+    question: "What is the correct HTML element for playing video files??",
+    answer_1: "<media>>",
+    answer_2: "<video>>",
+    answer_3: "<movie>",
+    answer_4: "<cinema>",
+    right_answer: "2",
+  },
+  {
+    question: "Which property is used to change the font of an element?",
+    answer_1: "font-style",
+    answer_3: "font-family",
+    answer_2: "font-weight",
+    answer_4: "font-fat",
+    right_answer: "3",
+  },
+  {
+    question: "What is the default value of the position property?",
+    answer_1: "static",
+    answer_2: "absolute",
+    answer_3: "relative",
+    answer_4: "fixed",
+    right_answer: "1",
+  },
+];
+
+let currentQuestion = 0;
+
+function init() {
+  document.getElementById(`all-questions`).innerHTML = quizJson.length;
+  showQuestion();
 }
 
-function start() {
-  let CardBody = document.getElementById(`cardBody`);
-
-  CardBody.innerHTML = `
-  <div class="startDiv" onclick="startQuiz()">START</div>
-  `;
-}
-
-let n = 1;
-function questionFooter() {
-  let questionFooter = document.getElementById(`questionFooter`);
-  questionFooter.innerHTML = ``;
-
-  questionFooter.innerHTML += `
-    <div class="footer_counter">
-    <b>${n}</b> of <b id="array_lengh"></b>questions
-    </div>
-    <a onclick="checkAnswer()" href="javascript: void(0)" class="btn btn-primary">Next</a>
-    `;
-
-  document.getElementById(`array_lengh`).innerHTML = `${quizJson.length}`;
-}
-
-let i = 0;
-
-function renderQuestionAnswer() {
-  let CardBody = document.getElementById(`cardBody`);
-  CardBody.innerHTML = ``;
-
-  CardBody.innerHTML += ` 
-        <h5 class="card-title">${quizJson[i].question}</h5>
-        <div onclick="saveAnswer('answer_1')" id="a1" class="card mb-2">
-                <div id="answerCard" class="card-body">
-                ${quizJson[i].answer_1}
-                </div>
-        </div>
-        <div onclick="saveAnswer('answer_2')" id="a2" class="card mb-2">
-                <div id="answerCard" class="card-body">
-                ${quizJson[i].answer_2}
-                </div>
-        </div>
-        <div onclick="saveAnswer('answer_3')" id="a3" class="card mb-2">
-                <div id="answerCard" class="card-body">
-                ${quizJson[i].answer_3}
-                </div>
-        </div>
-        <div onclick="saveAnswer('answer_4')" id="a4" class="card mb-2">
-                <div id="answerCard" class="card-body">
-                ${quizJson[i].answer_4}
-                </div>
-        </div>
-        `;
-}
-
-function changeColorOfSelectedDiv(selection) {
-  let lastCharSelection = +selection.slice(-1);
-  if (lastCharSelection == "1") {
-    document.getElementById("a1").classList.add("changeColor");
-    removeColor("a2", "a3", "a4");
-  }
-  if (lastCharSelection == "2") {
-    document.getElementById("a2").classList.add("changeColor");
-    removeColor("a1", "a3", "a4");
-  }
-  if (lastCharSelection == "3") {
-    document.getElementById("a3").classList.add("changeColor");
-    removeColor("a2", "a1", "a4");
-  }
-  if (lastCharSelection == "4") {
-    document.getElementById("a4").classList.add("changeColor");
-    removeColor("a2", "a3", "a1");
-  }
-}
-
-function removeColor(x, y, z) {
-  document.getElementById(x).classList.remove("changeColor");
-  document.getElementById(y).classList.remove("changeColor");
-  document.getElementById(z).classList.remove("changeColor");
-}
-
-function checkAnswer() {
-  collectionOfQuestionSelections.push(saveSelection[0]);
-
-  if (n == 8 && i == 7) {
-    n = 1;
-    i = 0;
-    document.getElementById(`result`).classList.remove(`d-none`);
-    renderResult();
-  } else {
-    n++;
-    i++;
-  }
-
-  saveCollectedAnswers();
-  loadAnswerCollection();
-  onload();
-}
-
-function renderResult() {
-  let resultDiv = document.getElementById(`result`);
-  resultDiv.innerHTML = ``;
-  for (let j = 0; j < collectionOfQuestionSelections.length; j++) {
-    let finalSelection = collectionOfQuestionSelections[j];
-    resultDiv.innerHTML += `
-    <button id="btn-close" class="btn btn-primary" onclick="closeResultDiv()">Close</button>
-    <div>${finalSelection}</div>
-    `;
-  }
-}
-
-function closeResultDiv() {
-  document.getElementById(`result`).classList.add(`d-none`);
-}
-
-// --------------------------------------------------------------------------------------------------------------
-
-function saveAnswer(selection) {
-  saveSelection.splice(0, 1);
-  saveSelection.push(selection);
-
-  let saveSelectedAnswer = JSON.stringify(selection);
-  localStorage.setItem("selection", saveSelectedAnswer);
-  changeColorOfSelectedDiv(selection);
-}
-
-function saveCollectedAnswers() {
-  let saveCollection = JSON.stringify(collectionOfQuestionSelections);
-  localStorage.setItem("collectionOfQuestionSelections", saveCollection);
-}
-
-function loadAnswerCollection() {
-  let saveCollection = localStorage.getItem(`collectionOfQuestionSelections`);
-
-  if (saveCollection) {
-    collectionOfQuestionSelections = JSON.parse(saveCollection);
-  }
+function showQuestion() {
+  let question = quizJson[currentQuestion];
+  document.getElementById(`question`).innerHTML = question["question"];
+  document.getElementById(`answer_1`).innerHTML = question["answer_1"];
+  document.getElementById(`answer_2`).innerHTML = question["answer_2"];
+  document.getElementById(`answer_3`).innerHTML = question["answer_3"];
+  document.getElementById(`answer_4`).innerHTML = question["answer_4"];
 }
